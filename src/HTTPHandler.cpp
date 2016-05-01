6648874b351a212bdfc7e58ad6f9465544fa9f93
Konrad <k.czarnota27@gmail.com>
2016-05-01 21:35:13 +0200

    Dodane parsowanie jsona z akcją do wykonania i tworzenie odpowiedzi

--- src/HTTPHandler.cpp
+++ src/HTTPHandler.cpp
@@ -1,1 +1,100 @@
 #include "HTTPHandler.hpp"
+
+
+void HTTPHandler::parse(std::string json)
+{
+    Json::Value root;
+    Json::Reader reader;
+    bool parsingSuccessful = reader.parse(json, root);
+
+    if (!parsingSuccessful)
+    {
+        std::string details = reader.getFormattedErrorMessages();
+        HTTPHandlerException httpHandlerException(HTTPHandlerException::Type::REQUEST_SYNTAX_ERROR);
+        httpHandlerException.setDescription(details);
+        throw httpHandlerException;
+    }
+
+    if(!root["command"].empty())
+        this->command = root["command"].asString();
+    else
+    {
+        HTTPHandlerException httpHandlerException(HTTPHandlerException::Type::REQUEST_NO_COMMAND);
+        httpHandlerException.setDescription("No command was specified in json file");
+        throw httpHandlerException;
+    }
+
+    if(!root["domains"].empty())
+    {
+        Json::Value jsonDomains = root["domains"];
+        for(int i = 0; i < jsonDomains.size(); ++i)
+            this->domains.push_back(jsonDomains[i].asString());
+    }
+    else
+    {
+        HTTPHandlerException httpHandlerException(HTTPHandlerException::Type::REQUEST_NO_DOMAINS);
+        httpHandlerException.setDescription("No domains were specified in json file");
+        throw httpHandlerException;
+    }
+}
+
+std::string HTTPHandler::chooseAction(std::string json)
+{
+    parse(json);
+
+    if(this->command == addAction)
+    {
+        for(std::vector<std::string>::iterator it = this->domains.begin(); it != this->domains.end(); ++it)
+        {
+            this->vecOfDomains.append(*it);
+            this->pair["domain"] = *it;
+            HTTPHandler::MessageStatus status = Database::getInstance().addDomain(*it);
+            this->pair["status"] = statusToString(status);
+            this->vecOfDomainStatusPairs.append(this->pair);
+        }
+    }
+    else if(this->command == removeAction)
+    {
+        for(std::vector<std::string>::iterator it = this->domains.begin(); it != this->domains.end(); ++it)
+        {
+            this->vecOfDomains.append(*it);
+            this->pair["domain"] = *it;
+            HTTPHandler::MessageStatus status = Database::getInstance().removeDomain(*it);
+            this->pair["status"] = statusToString(status);
+            this->vecOfDomainStatusPairs.append(this->pair);
+        }
+    }
+    else if(this->command == queryAction)
+    {
+        for(std::vector<std::string>::iterator it = this->domains.begin(); it != this->domains.end(); ++it)
+        {
+            this->vecOfDomains.append(*it);
+            this->pair["domain"] = *it;
+            HTTPHandler::MessageStatus status = Database::getInstance().getDomainStatus(*it);
+            this->pair["status"] = statusToString(status);
+            this->vecOfDomainStatusPairs.append(this->pair);
+        }
+    }
+    prepareResponse();
+}
+
+std::string HTTPHandler::statusToString(HTTPHandler::MessageStatus s)
+{
+    if (s == HTTPHandler::MessageStatus::OK)
+        return "ok";
+    else if (s == HTTPHandler::MessageStatus::ALREADY_IN_DATABASE)
+        return "already_in_database";
+    else if (s == HTTPHandler::MessageStatus::NO_IN_DATABASE)
+        return "no_in_database";
+    else if (s == HTTPHandler::MessageStatus::UNKNOWN)
+        return "unknown";
+    else if (s == HTTPHandler::MessageStatus::UNREACHABLE)
+        return "unreachable";
+}
+
+void HTTPHandler::prepareResponse()
+{
+    this->response["task"]["command"] = this->command;
+    this->response["task"]["domains"] = this->vecOfDomains;
+    this->response["result"] = vecOfDomainStatusPairs;
+}
--- src/HTTPHandler.hpp
+++ src/HTTPHandler.hpp
@@ -2,10 +2,60 @@
 #define DNS_CHECKER_HTTPHANDLER_H
 
 
+#include <string>
+#include <json/json.h>
+#include <json/writer.h>
+#include "Database.hpp"
+
+class HTTPHandler;
+
+class HTTPHandlerException
+{
+public:
+    enum Type
+    {
+        REQUEST_SYNTAX_ERROR,
+        REQUEST_NO_COMMAND,
+        REQUEST_NO_DOMAINS
+
+    };
+    const std::string getDescription() const {
+        return this->description;
+    }
+    void setDescription(const std::string description) {
+        this->description = description;
+    }
+private:
+    std::string description;
+    friend class HTTPHandler;
+    HTTPHandlerException(HTTPHandlerException::Type type)
+    {
+        this->type = type;
+    }
+    Type type;
+};
+
+
 class HTTPHandler
 {
 public:
     enum MessageStatus {OK, NO_IN_DATABASE, ALREADY_IN_DATABASE, UNREACHABLE, UNKNOWN};
+
+private:
+    const std::string addAction = "add";
+    const std::string removeAction = "remove";
+    const std::string queryAction = "query";
+    std::string command;
+    std::vector<std::string> domains;
+    Json::Value response;
+    Json::Value vecOfDomains;
+    Json::Value pair;
+    Json::Value vecOfDomainStatusPairs;
+
+    std::string chooseAction(std::string json);
+    void parse(std::string json);
+    std::string statusToString(HTTPHandler::MessageStatus s);
+    void prepareResponse();
 
 };
 
