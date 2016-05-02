321b81a30bd871187a13e6bede6be16f29d9f9e8
Radek <radek1236987@gmail.com>
2016-05-02 17:28:24 +0200

    DNSPooler

--- src/DNSPooler.cpp
+++ src/DNSPooler.cpp
@@ -1,5 +1,36 @@
-//
-// Created by radek on 02.05.16.
-//
-
 #include "DNSPooler.hpp"
+#include <chrono>
+
+DNSPooler::DNSPooler(int interval) {
+    this->interval = interval;
+}
+
+void DNSPooler::run() {
+    std::thread thread(&DNSPooler::pool, this);
+    thread.detach();
+}
+
+void DNSPooler::setInterval(int interval) {
+    this->interval = interval;
+}
+
+void DNSPooler::pool() {
+    while (true) {
+        this->refreshDomains();
+        std::chrono::seconds sleepTime(this->interval);
+        std::this_thread::sleep_for(sleepTime);
+    }
+}
+
+void DNSPooler::refreshDomains() {
+
+}
+
+
+
+
+
+
+
+
+
--- src/DNSPooler.hpp
+++ src/DNSPooler.hpp
@@ -1,12 +1,16 @@
-//
-// Created by radek on 02.05.16.
-//
-
 #ifndef DNS_CHECKER_DNSPOOLER_HPP
 #define DNS_CHECKER_DNSPOOLER_HPP
 
+#include <thread>
 
 class DNSPooler {
+    int interval;
+    void pool();
+    void refreshDomains();
+public:
+    DNSPooler(int interval);
+    void run();
+    void setInterval(int interval);
 
 };
 
