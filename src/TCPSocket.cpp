0f3353206928442b21fa1b9d22ac52d1ba4b2acd
Radek <radek1236987@gmail.com>
2016-06-01 23:25:50 +0200

    Add buffered IO in TCPSocket

--- src/TCPSocket.cpp
+++ src/TCPSocket.cpp
@@ -3,58 +3,52 @@
 /**
  * Default constructor for TCPSocket
  */
-TCPSocket::TCPSocket()
-{
+TCPSocket::TCPSocket() {
+    this->bufferNotEmpty = 0;
+    this->currentReadPos = 0;
     this->isConnected = 0;
     this->socketFileDescriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 }
 
-TCPSocket::TCPSocket(int socketFileDescriptor)
-{
+TCPSocket::TCPSocket(int socketFileDescriptor) {
     this->socketFileDescriptor = socketFileDescriptor;
 }
 
-TCPSocket::~TCPSocket()
-{
+TCPSocket::~TCPSocket() {
     close(this->socketFileDescriptor);
 }
 
-int TCPSocket::connect(SocketAddress & address)
-{
+int TCPSocket::connect(SocketAddress &address) {
     int status =
-        ::connect(this->socketFileDescriptor,
-                  address.toInternalAddressStructPointer(), address.getSize());
+            ::connect(this->socketFileDescriptor,
+                      address.toInternalAddressStructPointer(),
+                      address.getSize());
     return status;
 }
 
-int TCPSocket::bind(const SocketAddress & address)
-{
+int TCPSocket::bind(const SocketAddress &address) {
     int result =
-        ::bind(this->socketFileDescriptor,
-               address.toInternalAddressStructPointer(), address.getSize());
+            ::bind(this->socketFileDescriptor,
+                   address.toInternalAddressStructPointer(), address.getSize());
     return result;
 }
 
-int TCPSocket::listen(int backlog)
-{
+int TCPSocket::listen(int backlog) {
     ::listen(this->socketFileDescriptor, backlog);
 }
 
-int TCPSocket::accept()
-{
+int TCPSocket::accept() {
     fd_set rfds;
-    while (1)
-    {
+    while (1) {
         FD_ZERO(&rfds);
         FD_SET(this->socketFileDescriptor, &rfds);
-        int retval = select(this->socketFileDescriptor + 1, &rfds, nullptr, nullptr,
-                        &this->timeout);
-        if (retval > 0)
-        {
+        int retval = select(this->socketFileDescriptor + 1, &rfds, nullptr,
+                            nullptr,
+                            &this->timeout);
+        if (retval > 0) {
             return ::accept(this->socketFileDescriptor, 0, 0);
         }
-        else
-        {
+        else {
             /*Timeout or error*/
             TimeoutException e;
             throw e;
@@ -62,97 +56,81 @@ int TCPSocket::accept()
     }
 }
 
-TCPSocket & TCPSocket::operator<<(const std::string & data)
-{
+TCPSocket &TCPSocket::operator<<(const std::string &data) {
     send(this->socketFileDescriptor, data.c_str(), data.size(), 0);
     return *this;
 }
 
-std::string TCPSocket::readLine()
-{
-	std::string data = "";
-    fd_set rfds;
-    int retval;
-    ssize_t recive_size = 0;
+std::string TCPSocket::readLine() {
+    std::string data = "";
     char buffer;
-    int numAttempts = 2;
-    while (1)
-    {
-        FD_ZERO(&rfds);
-        FD_SET(this->socketFileDescriptor, &rfds);
-        retval = select(this->socketFileDescriptor + 1, &rfds, nullptr, nullptr,
-                        &this->timeout);
-        if (retval > 0)
-        {
-            recive_size =
-                recv(this->socketFileDescriptor, &buffer, 1, 0);
-            data += buffer;
-			if(buffer == '\n')
-				break;
-            buffer = '\0';
-            numAttempts = 2;
+    while (1) {
+        buffer = this->readByte();
+        if (buffer == EOF) {
+            break;
         }
-        else if (numAttempts > 0)
-        {
-            numAttempts--;
-        }
-        else
-        {
-            /*Timeout or error*/
-            TimeoutException e;
-            throw e;
-        }
+        data += buffer;
+        if (buffer == '\n')
+            break;
+    }
+    if (data == "") {
+        TimeoutException e;
+        throw e;
     }
     return data;
 }
 
-std::string TCPSocket::read_from_socket(int size)
-{
-	std::string data;
-    fd_set rfds;
-    int retval;
-    ssize_t recive_size = 0;
-    char buffer[size+1];
-    int numAttempts = 2;
-    while (recive_size < size)
-    {
-        FD_ZERO(&rfds);
-        FD_SET(this->socketFileDescriptor, &rfds);
-        retval = select(this->socketFileDescriptor + 1, &rfds, nullptr, nullptr,
-                        &this->timeout);
-        if (retval > 0)
-        {
-            recive_size +=
-                recv(this->socketFileDescriptor, buffer, size, 0);
-            buffer[size] = '\0';
-			std::string str(buffer);
-            data += str;
-            numAttempts = 2;
+std::string TCPSocket::read_from_socket(int size) {
+    std::string data;
+    while (size--) {
+        char c = this->readByte();
+        if (c == EOF) {
+            break;
         }
-        else if (numAttempts > 0)
-        {
-            numAttempts--;
-        }
-        else
-        {
-            /*Timeout or error*/
-            TimeoutException e;
-            throw e;
-        }
+        data += c;
     }
-	return data;
+    if (data == "") {
+        TimeoutException e;
+        throw e;
+    }
+    return data;
 }
 
 
-void TCPSocket::serveForever(int port)
-{
+void TCPSocket::serveForever(int port) {
     this->bindAll(port);
     this->listen();
 }
 
-void TCPSocket::bindAll(int port)
-{
+void TCPSocket::bindAll(int port) {
     SocketAddress address;
     address.setPort(port);
     this->bind(address);
 }
+
+/*
+ * Read from socket with bufferd IO
+ */
+char TCPSocket::readByte() {
+    if (this->bufferNotEmpty) {
+        if (this->currentReadPos < this->bufferSize) {
+            return this->buffer[this->currentReadPos++];
+        }
+        this->bufferNotEmpty = false;
+    }
+    ssize_t receive_size = 0;
+    while (1) {
+        receive_size = recv(this->socketFileDescriptor, &(this->buffer), 4096,
+                           0);
+        if (receive_size > 0) {
+            this->bufferNotEmpty = true;
+            this->currentReadPos = 0;
+            this->bufferSize = (int) receive_size;
+            return this->readByte();
+        }
+        else if (receive_size < 0) {
+            return EOF;
+        }
+    }
+}
+
--- src/TCPSocket.hpp
+++ src/TCPSocket.hpp
@@ -13,7 +13,11 @@
 class TCPSocket : public Socket
 {
 private:
+    char buffer[4096];
+    bool bufferNotEmpty;
     bool isConnected;
+    int bufferSize;
+    int currentReadPos;
 
 public:
     enum TCPException {
@@ -32,6 +36,8 @@ public:
 	std::string read_from_socket(int size);
     void serveForever(int port);
     void bindAll(int port);
+    char readByte();
+
 };
 
 #endif /* TCPSOCKET_HPP_ */
