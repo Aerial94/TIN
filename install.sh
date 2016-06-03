d05ba357f71a49c6fe76d5456dd2a9cc819a8ae1
Radek <radek1236987@gmail.com>
2016-06-03 22:23:25 +0200

    Update install.sh
    Color output added

--- install.sh
+++ install.sh
@@ -4,8 +4,19 @@ sudo aptitude update
 sudo aptitude -y install cmake build-essential libjsoncpp-dev git
 git clone "https://github.com/Aerial94/TIN" dns-checker
 cd dns-checker
+echo -e "\e[36mCreate build dir\e[0m"
 mkdir build
 cd build
+echo -e "\e[36mInvoking cmake\e[0m"
 cmake ..
+echo -e "\e[36mInvoking make\e[0m"
 make dns-checker
-cp ../conf/configuration.json .
+cd ..
+echo -e "\e[36mCreate bin dir\e[0m"
+mkdir bin
+echo -e "\e[36mCoping dns-checker binary to bin dir\e[0m"
+cp build/dns-checker bin
+echo -e "\e[36mCoping configuration to bin dir\e[0m"
+cp conf/configuration.json bin
+echo -e "\e[36mEntering bin dir\e[0m"
+cd bin
